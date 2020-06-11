65795f12f185c2d1ba338e08b973bd209cf1f7ae
Sepp Koster <sepp.koster@gmx.ch>
2020-06-11 21:39:05 +0200

    minimal view

---
+++ public/js/models/note.js
@@ -0,0 +1,10 @@
+class Note {
+  constructor(title, description, importance, dueAt) {
+    this.title = title;
+    this.description = description;
+    this.importance = importance;
+    this.dueAt = dueAt;
+    this.createdAt = new Date();
+    this.finished = false;
+  }
+}
